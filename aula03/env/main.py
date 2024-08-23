208d0b0824b700f7eedbdd29f08c7198398de5e6
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-08-23 07:25:45 -0300

    aula04 - exerc01

--- aula03/env/main.py
+++ aula03/env/main.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', uma_variavel_no_html = 'Licas', teste = 'Teste de variável')
+    return render_template('index.html', uma_variavel_no_html = 'Lucas', i = 0)
 
-@app.route('/pag1')
+@app.route('/teste1')
 def pag1():
-    return render_template('pag1.html')
+    return render_template('teste1.html',num1 = 3, num2 = 5)
 
-@app.route('/pag2')
+@app.route('/teste2')
 def pag2():
-    return render_template('pag2.html')
+    return render_template('teste2.html', titulo_pag2 = "Olá mundo",  url_imagem = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png')
 
 app.run(debug=True)
