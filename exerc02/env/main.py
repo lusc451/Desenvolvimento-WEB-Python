1cb0bd3a437b7f980f946eb7aff6b8528819bd22
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-10 19:03:44 -0300

    Exercício02

--- exerc02/env/main.py
+++ exerc02/env/main.py
@@ -5,6 +5,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/reservation')
+def reservation():
+    return render_template('reservation.html')
+
+@app.route('/special-dishes')
+def special_dishes():
+    return render_template('special-dishes.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 app.run(debug=True)
