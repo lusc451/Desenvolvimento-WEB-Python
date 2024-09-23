7698dd03d46f5f03b934e05af030594410a75355
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-23 20:10:28 -0300

    Aula 7

--- aula07/env/app.py
+++ aula07/env/app.py
@@ -11,6 +11,14 @@ def calcular_imc():
     altura = float(request.form['txt_altura'])
     peso = float(request.form['txt_peso'])
     imc = peso / (altura * altura)
-    return render_template('imc.html', result_iml = str(imc))
+    return render_template('imc.html', result_imc = imc)
 
-app.run()
+@app.route('/calcular_imc_get') #Não informar o método, entende-se que será GET (PADRÃO)
+def calcular_imc_get():
+    args = request.args
+    altura = float(args.get('txt_altura'))
+    peso = float(args.get('txt_peso'))
+    imc = peso / (altura * altura)
+    return render_template('imc.html', result_imc = imc)
+
+app.run()    
