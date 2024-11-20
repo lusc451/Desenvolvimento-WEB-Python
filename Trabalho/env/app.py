b4e9176748078f4d114b1d5c908a74ec5fb226b1
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-11-20 13:59:24 -0300

    Trabalho

--- Trabalho/env/app.py
+++ Trabalho/env/app.py
@@ -121,35 +121,141 @@ def cursos():
     )
     cursor = conn.cursor(dictionary=True)
 
+    # Inserir novo curso
     if request.method == 'POST':
         nome = request.form['nome']
-        disciplinas = request.form.getlist('disciplinas')
         cursor.execute("INSERT INTO LucasAntunes_tbcursos (nome) VALUES (%s)", (nome,))
-        curso_id = cursor.lastrowid
-        for disciplina_id in disciplinas:
-            cursor.execute("INSERT INTO LucasAntunes_tbcursos_disciplinas (curso_id, disciplina_id) VALUES (%s, %s)", (curso_id, disciplina_id))
         conn.commit()
         cursor.close()
         conn.close()
         return redirect(url_for('cursos'))
 
-    cursor.execute("SELECT * FROM LucasAntunes_tbdisciplinas")
-    disciplinas = cursor.fetchall()
+    # Buscar cursos e disciplinas
     cursor.execute("""
-        SELECT cursos.id, cursos.nome, GROUP_CONCAT(disciplinas.nome SEPARATOR ', ') AS disciplinas
-        FROM LucasAntunes_tbcursos AS cursos
-        LEFT JOIN LucasAntunes_tbcursos_disciplinas AS cd ON cursos.id = cd.curso_id
-        LEFT JOIN LucasAntunes_tbdisciplinas AS disciplinas ON cd.disciplina_id = disciplinas.id
-        GROUP BY cursos.id
+        SELECT 
+            c.id AS curso_id, 
+            c.nome AS curso_nome, 
+            GROUP_CONCAT(d.nome SEPARATOR ', ') AS disciplinas
+        FROM LucasAntunes_tbcursos AS c
+        LEFT JOIN LucasAntunes_tbcursos_disciplinas AS cd ON c.id = cd.curso_id
+        LEFT JOIN LucasAntunes_tbdisciplinas AS d ON cd.disciplina_id = d.id
+        GROUP BY c.id
     """)
     cursos = cursor.fetchall()
+
     cursor.close()
     conn.close()
-    return render_template('cursos.html', cursos=cursos, disciplinas=disciplinas)
+    return render_template('cursos.html', cursos=cursos)
+
+
+
+@app.route('/cursos/edit/<int:id>', methods=['GET', 'POST'])
+def edit_curso(id):
+    conn = mysql.connector.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE')
+    )
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'POST':
+        # Atualizando o nome do curso
+        nome = request.form['nome']
+        cursor.execute("UPDATE LucasAntunes_tbcursos SET nome = %s WHERE id = %s", (nome, id))
+
+        # Atualizando as disciplinas associadas ao curso
+        disciplinas = request.form.getlist('disciplinas')
+        cursor.execute("DELETE FROM LucasAntunes_tbcursos_disciplinas WHERE curso_id = %s", (id,))
+        for disciplina_id in disciplinas:
+            cursor.execute(
+                "INSERT INTO LucasAntunes_tbcursos_disciplinas (curso_id, disciplina_id) VALUES (%s, %s)",
+                (id, disciplina_id)
+            )
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('cursos'))
+
+    # Consultando dados do curso
+    cursor.execute("SELECT * FROM LucasAntunes_tbcursos WHERE id = %s", (id,))
+    curso = cursor.fetchone()
+
+    # Consultando todas as disciplinas
+    cursor.execute("SELECT * FROM LucasAntunes_tbdisciplinas")
+    disciplinas = cursor.fetchall()
+
+    # Consultando disciplinas já associadas ao curso
+    cursor.execute("SELECT disciplina_id FROM LucasAntunes_tbcursos_disciplinas WHERE curso_id = %s", (id,))
+    curso_disciplinas = [row['disciplina_id'] for row in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
+    return render_template('edit_curso.html', curso=curso, disciplinas=disciplinas, curso_disciplinas=curso_disciplinas)
+
+
+
+@app.route('/cursos/delete/<int:id>', methods=['POST'])
+def delete_curso(id):
+    conn = mysql.connector.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE')
+    )
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM LucasAntunes_tbcursos WHERE id = %s", (id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return redirect(url_for('cursos'))
+
 
 # Cadastro de Professores
 @app.route('/professores', methods=['GET', 'POST'])
 def professores():
+    conn = mysql.connector.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE')
+    )
+    cursor = conn.cursor(dictionary=True)
+
+    # Inserir novo professor
+    if request.method == 'POST':
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+        cursor.execute(
+            "INSERT INTO LucasAntunes_tbprofessores (nome, telefone, usuario, senha) VALUES (%s, %s, %s, %s)",
+            (nome, telefone, usuario, senha)
+        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('professores'))
+
+    # Buscar professores
+    search = request.args.get('search', '')
+    if search:
+        cursor.execute("SELECT * FROM LucasAntunes_tbprofessores WHERE nome LIKE %s", (f"%{search}%",))
+    else:
+        cursor.execute("SELECT * FROM LucasAntunes_tbprofessores")
+    professores = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return render_template('professores.html', professores=professores, search=search)
+
+
+@app.route('/professores/edit/<int:id>', methods=['GET', 'POST'])
+def edit_professor(id):
     conn = mysql.connector.connect(
         user=os.getenv('MYSQL_USER'),
         password=os.getenv('MYSQL_PASSWORD'),
@@ -164,31 +270,38 @@ def professores():
         telefone = request.form['telefone']
         usuario = request.form['usuario']
         senha = request.form['senha']
-        disciplinas = request.form.getlist('disciplinas')
-        cursor.execute("INSERT INTO LucasAntunes_tbprofessores (nome, telefone, usuario, senha) VALUES (%s, %s, %s, %s)", 
-                       (nome, telefone, usuario, senha))
-        professor_id = cursor.lastrowid
-        for disciplina_id in disciplinas:
-            cursor.execute("INSERT INTO LucasAntunes_tbprofessores_disciplinas (professor_id, disciplina_id) VALUES (%s, %s)", 
-                           (professor_id, disciplina_id))
+        cursor.execute(
+            "UPDATE LucasAntunes_tbprofessores SET nome = %s, telefone = %s, usuario = %s, senha = %s WHERE id = %s",
+            (nome, telefone, usuario, senha, id)
+        )
         conn.commit()
         cursor.close()
         conn.close()
         return redirect(url_for('professores'))
 
-    cursor.execute("SELECT * FROM LucasAntunes_tbdisciplinas")
-    disciplinas = cursor.fetchall()
-    cursor.execute("""
-        SELECT professores.id, professores.nome, professores.telefone, GROUP_CONCAT(disciplinas.nome SEPARATOR ', ') AS disciplinas
-        FROM LucasAntunes_tbprofessores AS professores
-        LEFT JOIN LucasAntunes_tbprofessores_disciplinas AS pd ON professores.id = pd.professor_id
-        LEFT JOIN LucasAntunes_tbdisciplinas AS disciplinas ON pd.disciplina_id = disciplinas.id
-        GROUP BY professores.id
-    """)
-    professores = cursor.fetchall()
+    cursor.execute("SELECT * FROM LucasAntunes_tbprofessores WHERE id = %s", (id,))
+    professor = cursor.fetchone()
     cursor.close()
     conn.close()
-    return render_template('professores.html', professores=professores, disciplinas=disciplinas)
+    return render_template('edit_professor.html', professor=professor)
+
+
+@app.route('/professores/delete/<int:id>', methods=['POST'])
+def delete_professor(id):
+    conn = mysql.connector.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE')
+    )
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM LucasAntunes_tbprofessores WHERE id = %s", (id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return redirect(url_for('professores'))
+
 
 # Cadastro de Alunos
 @app.route('/alunos', methods=['GET', 'POST'])
@@ -202,30 +315,90 @@ def alunos():
     )
     cursor = conn.cursor(dictionary=True)
 
+    # Inserir novo aluno
     if request.method == 'POST':
         nome = request.form['nome']
         cpf = request.form['cpf']
         endereco = request.form['endereco']
         senha = request.form['senha']
-        curso_id = request.form['curso']
-        cursor.execute("INSERT INTO LucasAntunes_tbalunos (nome, cpf, endereco, senha, curso_id) VALUES (%s, %s, %s, %s, %s)", 
-                       (nome, cpf, endereco, senha, curso_id))
+        curso_id = request.form['curso_id']
+        cursor.execute(
+            "INSERT INTO LucasAntunes_tbalunos (nome, cpf, endereco, senha, curso_id) VALUES (%s, %s, %s, %s, %s)",
+            (nome, cpf, endereco, senha, curso_id)
+        )
         conn.commit()
         cursor.close()
         conn.close()
         return redirect(url_for('alunos'))
 
+    # Buscar alunos
+    search = request.args.get('search', '')
+    if search:
+        cursor.execute("""
+            SELECT a.id, a.nome, a.cpf, a.endereco, c.nome AS curso_nome
+            FROM LucasAntunes_tbalunos AS a
+            LEFT JOIN LucasAntunes_tbcursos AS c ON a.curso_id = c.id
+            WHERE a.nome LIKE %s
+        """, (f"%{search}%",))
+    else:
+        cursor.execute("""
+            SELECT a.id, a.nome, a.cpf, a.endereco, c.nome AS curso_nome
+            FROM LucasAntunes_tbalunos AS a
+            LEFT JOIN LucasAntunes_tbcursos AS c ON a.curso_id = c.id
+        """)
+    alunos = cursor.fetchall()
+
+    # Consultar lista de cursos para o formulário
     cursor.execute("SELECT * FROM LucasAntunes_tbcursos")
     cursos = cursor.fetchall()
-    cursor.execute("""
-        SELECT alunos.id, alunos.nome, alunos.cpf, alunos.endereco, cursos.nome AS curso
-        FROM LucasAntunes_tbalunos AS alunos
-        LEFT JOIN LucasAntunes_tbcursos AS cursos ON alunos.curso_id = cursos.id
-    """)
-    alunos = cursor.fetchall()
+
     cursor.close()
     conn.close()
-    return render_template('alunos.html', alunos=alunos, cursos=cursos)
+    return render_template('alunos.html', alunos=alunos, cursos=cursos, search=search)
+
+@app.route('/alunos/edit/<int:id>', methods=['GET', 'POST'])
+def edit_aluno(id):
+    conn = mysql.connector.connect(
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE')
+    )
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'POST':
+        # Obtendo dados do formulário
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        endereco = request.form['endereco']
+        senha = request.form['senha']
+        curso_id = request.form['curso_id']
+
+        # Atualizando o registro no banco de dados
+        cursor.execute("""
+            UPDATE LucasAntunes_tbalunos 
+            SET nome = %s, cpf = %s, endereco = %s, senha = %s, curso_id = %s 
+            WHERE id = %s
+        """, (nome, cpf, endereco, senha, curso_id, id))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+        return redirect(url_for('alunos'))
+
+    # Consultando dados do aluno para edição
+    cursor.execute("SELECT * FROM LucasAntunes_tbalunos WHERE id = %s", (id,))
+    aluno = cursor.fetchone()
+
+    # Consultando lista de cursos para o dropdown
+    cursor.execute("SELECT * FROM LucasAntunes_tbcursos")
+    cursos = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return render_template('edit_aluno.html', aluno=aluno, cursos=cursos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
